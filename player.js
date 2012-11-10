79d719cefe81a04f235c4c32c0ebfa12b8bb85fb
stefan <stefan@superbox.(none)>
2012-11-10 05:36:50 -0800

    minor key functionality added

--- player.js
+++ player.js
@@ -6,7 +6,11 @@ var MusicPlayer = function () {
 					var column = grid[i];
 					for (var j = 0; j<16; j++) {
 						if (column[j].enabled) {
-							tones.pulseSound(majorChordNotes[15-j], audiolet);
+							if (majorKey) {
+								tones.pulseSound(majorChordNotes[15-j], audiolet);
+							} else {
+								tones.pulseSound(minorChordNotes[15-j], audiolet);
+							}
 						}
 					}
 				};
